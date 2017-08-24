39c195aab3960f503b73108459753bf5dc804e08
Jose Diaz <jediazse@gmail.com>
2017-08-24 16:20:55 -0400

    Adding VSCode settings

--- webpack.config.js
+++ webpack.config.js
@@ -3,26 +3,26 @@ const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 const config = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: 'babel-loader'
-            }
-        ]
-    },
-    plugins: [
-        new UglifyJSPlugin(),
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        })
-    ]
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: 'babel-loader'
+			}
+		]
+	},
+	plugins: [
+		new UglifyJSPlugin(),
+		new HtmlWebpackPlugin({
+			template: './src/index.html'
+		})
+	]
 }
 
 module.exports = config
