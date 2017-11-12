65ffd7b4bb34e3b12bcc374e7527d5cd2c38f4b0
josediazse <jediazse@gmail.com>
2017-11-12 16:40:10 -0500

    Adding defaultProps to some components

--- src/components/App.js
+++ src/components/App.js
@@ -6,12 +6,16 @@ import TodoList from './TodoList'
 import styles from '../styles.css'
 
 export default class App extends Component {
+  state = {
+    todoItems: ['Item 1', 'Item 2']
+  }
+
   render() {
     return (
       <div style={styles} className="container">
         <InputForm />
         <FilterItems />
-        <TodoList />
+        <TodoList todoItems={this.state.todoItems} />
       </div>
     )
   }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react'
 import TodoItem from './TodoItem'
+import { array } from 'prop-types'
 
 export default class TodoList extends Component {
   state = {
-    todoItems: ['Item 1', 'Item 2']
+    todoItems: this.props.todoItems
+  }
+
+  static propTypes = {
+    todoItems: array
+  }
+
+  static defaultProps = {
+    todoItems: []
   }
 
   render() {
     return (
       <div>
         <ul>
-          {this.state.todoItems ? (
+          {this.state.todoItems.length > 0 ? (
             this.state.todoItems.map(todo => {
               return <TodoItem key={todo} text={todo} toggleFunc={() => {}} />
             })
