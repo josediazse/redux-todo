d55c1d587872cbc01487a990e4a10e78945f6b97
josediazse <jediazse@gmail.com>
2017-12-08 01:31:18 -0500

    Adding Counter component.

--- src/index.js
+++ src/index.js
@@ -2,14 +2,53 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import App from './components/App'
-import MyReduxApp from './redux101'
-
-MyReduxApp()
+import { createStore } from 'redux'
 
 const MyApp = () => (
   <MuiThemeProvider>
     <App />
   </MuiThemeProvider>
 )
-
 ReactDOM.render(<MyApp />, document.getElementById('root'))
+
+const Counter = ({ value, onIncrement, onDecrement }) => (
+  <div>
+    <h1>{value}</h1>
+    <button onClick={onIncrement}>+</button>
+    <button onClick={onDecrement}>-</button>
+  </div>
+)
+
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1
+    case 'DECREMENT':
+      return state - 1
+    default:
+      return state
+  }
+}
+const store = createStore(counter)
+
+const render = () => {
+  ReactDOM.render(
+    <Counter
+      value={store.getState()}
+      onIncrement={() => {
+        store.dispatch({
+          type: 'INCREMENT'
+        })
+      }}
+      onDecrement={() => {
+        store.dispatch({
+          type: 'DECREMENT'
+        })
+      }}
+    />,
+    document.getElementById('redux')
+  )
+}
+
+store.subscribe(render)
+render()
