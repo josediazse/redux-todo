5446a75f6a630738389e7b061932275af65f70a2
josediazse <jediazse@gmail.com>
2017-12-08 02:14:34 -0500

    Adding initial Jest unit-tests for reducer.

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -1,7 +1,38 @@
 // import sum from '../src/components/App'
+import deepFreeze from 'deep-freeze'
 
-test('assert true', () => {
-  const expected = 10;
-  const result = 10;
-  expect(result).toBe(expected)
-});
+const todos = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [
+        ...state,
+        {
+          id: action.id,
+          text: action.text,
+          completed: false
+        }
+      ]
+    default:
+      return state
+  }
+}
+
+test('adds todo', () => {
+  const stateBefore = []
+  const action = {
+    type: 'ADD_TODO',
+    id: 0,
+    text: 'Learn Redux'
+  }
+  const stateAfter = [
+    {
+      id: 0,
+      text: 'Learn Redux',
+      completed: false
+    }
+  ]
+  deepFreeze(stateBefore)
+  deepFreeze(action)
+
+  expect(todos(stateBefore, action)).toEqual(stateAfter)
+})
