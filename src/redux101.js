e503f54330361513c5b87c354511d5ea8625a000
josediazse <jediazse@gmail.com>
2017-12-08 00:31:28 -0500

    Implement createStore function.

--- src/redux101.js
+++ src/redux101.js
@@ -1,4 +1,30 @@
-import { createStore } from 'redux'
+// import { createStore } from 'redux'
+const createStore = reducer => {
+  let state
+  let listeners = []
+
+  const getState = () => state
+
+  const dispatch = action => {
+    state = reducer(state, action)
+    listeners.forEach(listener => listener())
+  }
+
+  const subscribe = listener => {
+    listeners.push(listener)
+    return () => {
+      listeners = listeners.filter(l => l !== listener)
+    }
+  }
+
+  dispatch({})
+
+  return {
+    getState,
+    dispatch,
+    subscribe
+  }
+}
 
 export default function() {
   const counter = (state = 0, action) => {
