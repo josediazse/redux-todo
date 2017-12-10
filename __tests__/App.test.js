fa6a29a731ae4d278052a292218dad06570dccf1
josediazse <jediazse@gmail.com>
2017-12-10 17:17:20 -0500

    Initial test cases for reducer actions.

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -12,12 +12,23 @@ const todos = (state = [], action) => {
           completed: false
         }
       ]
+    case 'TOGGLE_TODO':
+      return state.map(todo => {
+        if (todo.id !== action.id) {
+          return todo
+        }
+
+        return {
+          ...todo,
+          completed: !todo.completed
+        }
+      })
     default:
       return state
   }
 }
 
-test('adds todo', () => {
+test('add todo', () => {
   const stateBefore = []
   const action = {
     type: 'ADD_TODO',
@@ -31,6 +42,42 @@ test('adds todo', () => {
       completed: false
     }
   ]
+  deepFreeze(stateBefore)
+  deepFreeze(action)
+
+  expect(todos(stateBefore, action)).toEqual(stateAfter)
+})
+
+test('toggle todo', () => {
+  const stateBefore = [
+    {
+      id: 0,
+      text: 'Learn Redux',
+      completed: false
+    },
+    {
+      id: 1,
+      text: 'Go Shopping',
+      completed: false
+    }
+  ]
+  const action = {
+    type: 'TOGGLE_TODO',
+    id: 1
+  }
+  const stateAfter = [
+    {
+      id: 0,
+      text: 'Learn Redux',
+      completed: false
+    },
+    {
+      id: 1,
+      text: 'Go Shopping',
+      completed: true
+    }
+  ]
+
   deepFreeze(stateBefore)
   deepFreeze(action)
 
